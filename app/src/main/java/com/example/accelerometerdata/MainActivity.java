8e33d7eebaf58af26ee74c5fc183a7e3ca352668
Abhishek Garain <abhi211199@gmail.com>
2020-08-26 19:23:46 +0530

    added push methods for the sensor data

--- app/src/main/java/com/example/accelerometerdata/MainActivity.java
+++ app/src/main/java/com/example/accelerometerdata/MainActivity.java
@@ -8,13 +8,30 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor gyroscope,gravity,linear_accelerometer,magnetometer;
     private TextView acc,gyro,gra,magnet;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    final CollectionReference cities = db.collection("sensors");
+    final Map<String, ArrayList> data1 = new HashMap<>();
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.registerListener(MainActivity.this,linear_accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(MainActivity.this,magnetometer,SensorManager.SENSOR_DELAY_NORMAL);
 
+        mRunnable.run();
     }
 
     @Override
@@ -44,13 +62,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor t = sensorEvent.sensor;
         if(t.equals(gravity)) {
             gra.setText("Gravity\n" + sensorEvent.values[0] + "\n" + sensorEvent.values[1] + "\n" + sensorEvent.values[2] + "\n");
+
+            ArrayList arr=new ArrayList();
+            arr.add(sensorEvent.values[0]);arr.add(sensorEvent.values[1]);arr.add(sensorEvent.values[2]);
+            data1.put("gravity",arr);
+
         }else if(t.equals(linear_accelerometer)){
             acc.setText("Accelerometer\n"+sensorEvent.values[0] + "\n" + sensorEvent.values[1] + "\n" + sensorEvent.values[2] + "\n");
+
+            ArrayList arr=new ArrayList();
+            arr.add(sensorEvent.values[0]);arr.add(sensorEvent.values[1]);arr.add(sensorEvent.values[2]);
+            data1.put("accelerometer",arr);
+
         }else if(t.equals(magnetometer)){
             magnet.setText("Magnetometer\n"+sensorEvent.values[0] + "\n" + sensorEvent.values[1] + "\n" + sensorEvent.values[2] + "\n");
+
+            ArrayList arr=new ArrayList();
+            arr.add(sensorEvent.values[0]);arr.add(sensorEvent.values[1]);arr.add(sensorEvent.values[2]);
+            data1.put("magnetometer",arr);
+
         }else{
             gyro.setText("Gyroscope\n"+sensorEvent.values[0] + "\n" + sensorEvent.values[1] + "\n" + sensorEvent.values[2] + "\n");
+
+            ArrayList arr=new ArrayList();
+            arr.add(sensorEvent.values[0]);arr.add(sensorEvent.values[1]);arr.add(sensorEvent.values[2]);
+            data1.put("gyroscope",arr);
+
         }
+//        cities.document("sensors").set(data1);
 
     }
 
@@ -58,4 +97,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.e("hello",data1+"");
+            cities.document("sensorsdata").set(data1);
+            handler.postDelayed(this,500);
+        }
+    };
+
+
 }
